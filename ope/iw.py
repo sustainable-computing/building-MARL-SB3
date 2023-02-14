e345045f479fe12a6fbb887569f354d9895b5987
Aakash Sasikumar <aakash3697@gmail.com>
2023-02-14 14:00:04 -0700

    Update iw.py
    -fixed bug for IPW as well

--- ope/iw.py
+++ ope/iw.py
@@ -72,8 +72,10 @@ class InverseProbabilityWeighting(OPEBase):
         return np.log(value / (1 - value))
 
     def calculate_action_probability(self, dist, bin, action_bins):
-        bin_l = self.inv_sigmoid(action_bins[bin])
-        bin_r = self.inv_sigmoid(action_bins[bin+1])
+        if bin == len(action_bins-1):
+            bin_r = np.inf
+        else:
+            bin_r = self.inv_sigmoid(action_bins[bin+1])
         if self.univariate_action:
             integral = dist.cdf(torch.Tensor([bin_r])) - dist.cdf(torch.Tensor([bin_l]))
         else:
