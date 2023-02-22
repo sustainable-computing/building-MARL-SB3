9f482816db4dde127ea3008ccb74e2e994feb099
Aakash Sasikumar <aakash3697@gmail.com>
2023-02-22 14:35:43 -0700

    Update main.py
    - updated CLI to include online evaluation

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import typer
 from train import app as train_app
 from ope.commands import app as ope_app
+from evaluation.commands import app as eval_app
 
 
 app = typer.Typer()
 app.add_typer(train_app, name="train")
 app.add_typer(ope_app, name="ope")
+app.add_typer(eval_app, name="evaluate")
 
 
 if __name__ == "__main__":
