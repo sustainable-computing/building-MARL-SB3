2bedff4ccc0bf89e2a27459321cb75e2d97e73cc
Aakash Sasikumar <aakash3697@gmail.com>
2023-03-22 15:00:11 -0600

    Update tune_ppo.py
    - Fixed bug where a new sweep was made each time this command was called

--- tuneparams/tune_ppo.py
+++ tuneparams/tune_ppo.py
@@ -24,6 +24,7 @@ def tune_ppo_optimal_command(
         "configs/sweepconfigs/ppo_sweep_0.yaml",
         help="The location of the sweep config file"),
     sweep_name: str = typer.Option("ppo_sweep_0"),
+    sweep_id: str = typer.Option(""),
     energy_plus_loc: str = typer.Option(...),
     num_days: int = typer.Option(...),
     start_day: int = typer.Option(1),
@@ -36,18 +37,20 @@ def tune_ppo_optimal_command(
 ):
 
     sweep_config = load_config(sweep_config_loc)
-    sweep_id = wandb.sweep(sweep_config, project=wandb_project_name)
     log_dir = create_log_dir(log_dir, sweep_name)
     save_config(sweep_config, os.path.join(log_dir, "sweep_config.yaml"))
     default_args = locals()
 
-    wandb.agent(sweep_id, construct_train_function(default_args), count=1, project=wandb_project_name)
+    wandb.agent(sweep_id, construct_train_function(default_args),
+                count=1, project=wandb_project_name)
 
 
 def construct_train_function(default_args):
     def train_function():
-        wandb.init()
         config = {**default_args, **wandb.config}
+        wandb.init()
+        wandb.alert(title="Sweep Updates",
+                    text=f"Agent training started for sweep {config['sweep_id']}")
         config["use_wandb"] = False
         log_dir, _ = train_ppo.train(**config)
         progress_csv = os.path.join(log_dir, "progress.csv")
